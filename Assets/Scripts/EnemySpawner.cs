b44683045447fc2ea454147bbc7b4077ed4726a2
Ryan Stewart <stewart.rya@husky.neu.edu>
2017-11-15 21:52:48 -0500

    Added Oculus libraries.  Downloaded free assets.  Created game room.  Created enemy spawner

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+    public int maxEnemies = 3;
+    public float secondsBetweenNextSpawn = 5f;
+    List<GameObject> instantiatedEnemies = new List<GameObject>();
+
+    public float radius = 10;
+
+    public GameObject enemyPrefab;
+
+    Coroutine enemySpawnRoutine;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (enemySpawnRoutine == null && instantiatedEnemies.Count < maxEnemies)
+        {
+            enemySpawnRoutine = StartCoroutine(SpawnEnemy());
+        }
+	}
+
+    IEnumerator SpawnEnemy()
+    {
+        yield return new WaitForSeconds(secondsBetweenNextSpawn);
+
+        int angleDeg = Random.Range(0, 360);
+        float angleRad = Mathf.Deg2Rad * angleDeg;
+
+        Vector3 spawnLocation = new Vector3(radius * Mathf.Sin(angleRad), 
+                                            transform.position.y, 
+                                            radius * Mathf.Cos(angleRad));
+        Quaternion spawnRotation = Quaternion.AngleAxis(angleDeg + 180, Vector3.up);
+        instantiatedEnemies.Add(Instantiate(enemyPrefab, spawnLocation, spawnRotation));
+
+        enemySpawnRoutine = null;
+    }
+}
