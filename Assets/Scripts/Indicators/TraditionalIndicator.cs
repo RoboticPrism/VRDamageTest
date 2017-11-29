233de6540d9d638a15b0e8b888eb74465d88edb9
Derek Chan <derekchan7@gmail.com>
2017-11-29 03:15:15 -0500

    Minor beautify

--- Assets/Scripts/Indicators/TraditionalIndicator.cs
+++ Assets/Scripts/Indicators/TraditionalIndicator.cs
@@ -9,13 +9,13 @@ public class TraditionalIndicator : Indicator {
 
     // Use this for initialization
     void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+  	}
+
+  	// Update is called once per frame
+  	void Update () {
+
+  	}
 
     public new void AddDamagePoint(Enemy enemy)
     {
